bb891cfd97d4ce7e6eeaf3d98c057fd05160d01e
Mohammad Abdullateef <159006309+thenytmare@users.noreply.github.com>
2025-06-19 18:21:43 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
-
-int main() {
-    double num1, num2, result;
+int main() 
+{
+    float num1, num2, result;
     char operator;
 
     printf("Enter first number: ");
-    scanf("%lf", &num1);
+    scanf("%f", &num1);
 
     printf("Enter an operator (+, -, *, /, %%): ");
     scanf(" %c", &operator);
 
     printf("Enter second number: ");
-    scanf("%lf", &num2);
+    scanf("%f", &num2);
 
     switch (operator) {
         case '+':
             result = num1 + num2;
-            printf("Result: %.2lf\n", result);
+            printf("Result: %.2f\n", result);
             break;
         case '-':
             result = num1 - num2;
-            printf("Result: %.2lf\n", result);
+            printf("Result: %.2f\n", result);
             break;
         case '*':
             result = num1 * num2;
-            printf("Result: %.2lf\n", result);
+            printf("Result: %.2f\n", result);
             break;
         case '/':
             if (num2 == 0) {
-                printf("Error: Division by zero is not allowed.\n");
+                printf("!! Division by zero is not allowed !!\n");
             } else {
                 result = num1 / num2;
-                printf("Result: %.2lf\n", result);
+                printf("Result: %.2f\n", result);
             }
             break;
         case '%':
             if ((int)num2 == 0) {
-                printf("Error: Division by zero is not allowed.\n");
+                printf("!! Modulo by zero is not allowed !!\n");
             } else {
+                printf("!! Modulo will be calculated using integer parts only !!\n");
                 int int_num1 = (int)num1;
                 int int_num2 = (int)num2;
                 int mod_result = int_num1 % int_num2;
@@ -45,7 +46,7 @@ int main() {
             }
             break;
         default:
-            printf("Error: Invalid operator.\n");
+            printf("!! Invalid operator selected !!\n");
             break;
     }
 
